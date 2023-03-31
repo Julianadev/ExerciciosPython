c64fa4348a4f41cd34f350b16e98a465b2db535f
Juliana Ramos <juramos266@gmail.com>
2023-03-31 16:23:42 -0300

    Commit inicial

---
+++ ExercíciosSessão4/Exercicio1.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que leia um número inteiro e o imprima
+
+"""
+
+numero = 58
+print(numero)
---
+++ ExercíciosSessão4/Exercicio2.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que leia um número real e o imprima
+
+"""
+
+numeroReal = 58.5
+print(numeroReal)
