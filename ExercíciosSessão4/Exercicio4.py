a5f6ff2d063336f06fd8119b199e99f9852dc333
Juliana Ramos <juramos266@gmail.com>
2023-03-31 17:20:08 -0300

    update exercicios

---
+++ ExercíciosSessão4/Exercicio4.py
@@ -0,0 +1,6 @@
+"""
+Leia um número real e imprima o quadrado desse número.
+"""
+
+numero = 45.5
+print(numero.__pow__(2))
---
+++ ExercíciosSessão4/Exercicio5.py
@@ -0,0 +1,7 @@
+"""
+Leia um número real e imprima a quinta parte desse número
+"""
+
+numero = float(input('Digite um número: '))
+print(numero * (1/5))
+
