0fc31b2f49e101fc2eec22ef32e856eaf1c27421
Juliana Ramos <juramos266@gmail.com>
2023-04-11 01:19:07 -0300

    update

---
+++ ExercicioSessao6/Exercicio2.py
@@ -0,0 +1,26 @@
+
+
+# usando for
+for numero in range(1, 101):
+    print(numero)
+
+# usando while
+
+numero = 1
+while 1 <=100:
+    print(numero)
+    numero += 1
+
+# usando do while
+
+numero = 1
+while True:
+    print(numero)
+    numero +=1
+    if numero > 100:
+        break
+
+
+
+
+
